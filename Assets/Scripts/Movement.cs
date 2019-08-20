42d7584c8a5022788195b3a1787787e47494bf44
Johnny <johnnydcs@yahoo.com>
2019-08-19 22:05:20 -0700

    WASD Movement
    Changed movement keys to WASD via InputGetDown(KeyCode.W/A/S/D) and FixedUpdate() -> Update().
    Removed FrameCount within Update().

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -10,8 +10,6 @@ public class Movement : MonoBehaviour
 
     Vector3 position = Vector3.zero;
 
-    int frameCount = 0;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +20,9 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (frameCount % 5 == 0)
-        {
-            CheckMovement();
-        }
-
-        frameCount++;
-        if (frameCount == 60)
-            frameCount = 0;
+        CheckMovement();
     }
 
     void CheckMovement()
@@ -39,6 +30,19 @@ public class Movement : MonoBehaviour
         float vdir = 0.0f;
         float hdir = 0.0f;
 
+        if (Input.GetKeyDown(KeyCode.W))
+            vdir = 20.0f;
+
+        else if (Input.GetKeyDown(KeyCode.S))
+            vdir = -20.0f;
+
+        if (Input.GetKeyDown(KeyCode.D))
+            hdir = 20.0f;
+
+        else if (Input.GetKeyDown(KeyCode.A))
+            hdir = -20.0f;
+
+        /*
         // 20 units per square
         if (Input.GetAxis("Vertical") > 0)
             vdir = 20.0f;
@@ -49,6 +53,7 @@ public class Movement : MonoBehaviour
             hdir = 20.0f;
         else if (Input.GetAxis("Horizontal") < 0)
             hdir = -20.0f;
+        */
 
         Vector3 movedir = new Vector3(hdir, 0, vdir);
         movedir = transform.TransformDirection(movedir);
