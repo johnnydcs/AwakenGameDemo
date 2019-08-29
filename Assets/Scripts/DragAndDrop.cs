92331a6d0d12f49089675a9bd13d72cacc642b91
Johnny <johnnydcs@yahoo.com>
2019-08-29 01:42:51 -0700

    Cards and dragging
    Hand field and in use/play field made.
    Able to move cards between the two.
    Cards are placed last of the lineup after being dragged.
    2 scripts, for Dragging and for Dropping.
    
    To Do List:
    No more movement via WASD.
    Some cards can reposition the unit after attacks/effects.
    Need grid to snap units to center of blocks.
    Need deck of cards & draw from deck animation & player's hand of cards.

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -5,27 +5,29 @@ using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Transform originalParent = null;
+    public Transform originalParent = null;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        //Debug.Log("OnBeginDrag");
 
         originalParent = this.transform.parent;
-        this.transform.SetParent(this.transform.parent.parent);
+        this.transform.SetParent(this.transform.root);
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        //Debug.Log("OnDrag");
 
         this.transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
+        //Debug.Log("OnEndDrag");
 
         this.transform.SetParent(originalParent);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
---
+++ Assets/Scripts/DropArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropArea : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        DragAndDrop d = eventData.pointerDrag.GetComponent<DragAndDrop>();
+        if (d != null)
+        {
+            d.originalParent = this.transform;
+
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerEnter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("OnPointerExit");
+    }
+}
